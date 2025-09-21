2381698c8f5ca6a49cfbf0d167cf0a0591d552ea
Malav2364 <2303031057065@paruluniversity.ac.in>
2025-09-21 23:39:24 +0530

    Added components in Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { ContainerTextFlip } from '@/components/ui/container-text-flip' 
+import { Skiper26 } from '@/components/skiper26'
 
 export default function Home() {
   return (   
-    <>
-    <p>Start Your Next Chapter</p>    
-      <ContainerTextFlip 
+    <main>
+    <section className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex items-center">
+        <p className="text-7xl font-bold mr-4">Start Your Next Chapter</p>
+        <ContainerTextFlip
           words={["Travel.", "Explore.", "Discover.", "Create Memories."]}
         />
-    </>
+      </div>
+    </section>
+    <div className="fixed bottom-4 right-4">
+      <Skiper26 />
+    </div>
+  </main>
 
   )
 }
