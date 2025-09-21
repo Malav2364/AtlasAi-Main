7a9f7da3ac95cc8e5d03fc9a4cfc1d0bfc675ff4
Malav2364 <2303031057065@paruluniversity.ac.in>
2025-09-21 16:15:03 +0530

    Fixed hydration warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { ContainerTextFlip } from '@/components/ui/container-text-flip' 
 
 export default function Home() {
-  return (
-    <div>
-          <ContainerTextFlip
-                words={["Travel.", "Explore.", "Discover.", "Create Memories."]}
-            />
-    </div>
+  return (   
+    <>
+    <p>Start Your Next Chapter</p>    
+      <ContainerTextFlip 
+          words={["Travel.", "Explore.", "Discover.", "Create Memories."]}
+        />
+    </>
+
   )
 }
