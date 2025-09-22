803b91f95aa22ca6e3066622313960a8d9a9afd2
Malav2364 <2303031057065@paruluniversity.ac.in>
2025-09-22 15:57:38 +0530

    Added a section on Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { ContainerTextFlip } from '@/components/ui/container-text-flip';
-import { Skiper26 } from '@/components/skiper26';
+import React from 'react'
+import { ContainerTextFlip } from '@/components/ui/container-text-flip' 
+import { Skiper26 } from '@/components/skiper26'
 
 export default function Home() {
-  return (
+  return (   
     <main>
-      <section className="flex flex-col items-center justify-center min-h-screen">
-        <div className="flex items-center">
-          <p className="text-7xl font-bold mr-4">START YOUR NEW CHAPTER</p>
-          <ContainerTextFlip
-            words={["TRAVEL.", "EXPLORE.", "DISCOVER.", "CREATE MEMORIES."]}
-          />
-        </div>
-      </section>
-      <div className="fixed bottom-4 right-4">
-        <Skiper26 />
+    <section className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex items-center">
+        <p className="text-7xl font-bold mr-4">Start Your Next Chapter</p>
+        <ContainerTextFlip
+          words={["Travel.", "Explore.", "Discover.", "Create Memories."]}
+        />
       </div>
-    </main>
-  );
+    </section>
+    <div className="fixed bottom-4 right-4">
+      <Skiper26 />
+    </div>
+    <section>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        
+      </div>
+    </section>
+  </main>
+
+  )
 }
